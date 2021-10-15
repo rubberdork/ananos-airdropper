aa3a479fd806306db862f0008a13f7ea4120d389
rubberdork <rubberdork@example.com>
2021-10-15 13:02:43 +0800

    Make it easy to run test vs public

--- index.js
+++ index.js
@@ -1,23 +1,41 @@
 import { join } from 'path'
 import { readFile } from 'fs/promises'
-import { Server } from 'stellar-sdk'
+import { Server, Networks, Keypair } from 'stellar-sdk'
 import TOML from '@ltd/j-toml'
 import partition from 'lodash.partition'
 
 import { accountValidator } from './lib/account_validator.js'
 import { writeErrorReport } from './lib/write_error_report.js'
 
-const [config, addresses] = await Promise.all(
-  ['config.toml', 'addresses.txt'].map(f => readFile(f, 'utf8'))
-)
-const { asset } = TOML.parse(config)
-const server = new Server('https://horizon-testnet.stellar.org')
+const pub = process.env.PUBLIC === 'true'
+const BASEDIR = pub ? '.' : 'test'
+const NETWORK = pub ? 'PUBLIC' : 'TESTNET'
+const HORIZON_URL = `https://horizon${pub ? '' : '-testnet'}.stellar.org`
+
+const setupFiles = Promise.all([
+  'config.toml',
+  'addresses.txt',
+  'SECRETKEY'
+].map(f => readFile(join(BASEDIR, f), 'utf8')))
+
+setupFiles.catch((e) => {
+  console.error(`File "${e.path}" not found. Exiting…`)
+  process.exit(1)
+})
+
+const [config, addresses, secretkey] = await setupFiles
+
+const { asset, airdrop } = TOML.parse(config)
+asset.distributorKeypair = Keypair.fromSecret(secretkey.trim())
+
+const server = new Server(HORIZON_URL)
+const networkPassphrase = Networks[NETWORK]
 const validateAccount = accountValidator(server, asset)
 const accountIDs = addresses.split('\n').filter(s => !!s)
-
 const validatedAccountIDs = await Promise.all(accountIDs.map(validateAccount))
-const [validIDs, invalidIDs] = partition(validatedAccountIDs, acct => acct.success)
+const [validAccounts, invalidAccounts] = partition(validatedAccountIDs, acct => acct.success)
 
-const ERROR_REPORTS_FILE = join('.', 'reports', 'errors.csv')
-writeErrorReport(ERROR_REPORTS_FILE, invalidIDs, ['fedAddress', 'address', 'reason'])
-console.log(`Found problems with ${invalidIDs.length} addresses. Errors logged in ${ERROR_REPORTS_FILE}`)
+const VALIDATION_ERROR_REPORTS_FILE = join(BASEDIR, 'reports', 'errors.csv')
+writeErrorReport(VALIDATION_ERROR_REPORTS_FILE, invalidAccounts, ['address', 'fedAddress', 'reason'])
+console.log(`Found problems with ${invalidAccounts.length} addresses. Errors logged in ${VALIDATION_ERROR_REPORTS_FILE}`)
+
