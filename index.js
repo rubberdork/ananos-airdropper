1ebd021e79c39bab41822ce2fcf156f7d15cb7d4
rubberdork <rubberdork@example.com>
2021-11-04 14:48:17 +0800

    Wrap in async function; top-level await is a no-go

--- index.js
+++ index.js
@@ -16,82 +16,87 @@ const BASEDIR = pub ? '.' : 'test'
 const NETWORK = pub ? 'PUBLIC' : 'TESTNET'
 const HORIZON_URL = `https://horizon${pub ? '' : '-testnet'}.stellar.org`
 
-const setupFiles = Promise.all([
-  'config.toml',
-  'addresses.txt',
-  'SECRETKEY'
-].map(f => readFile(join(BASEDIR, f), 'utf8')))
 
-setupFiles.catch((e) => {
-  console.error(`File "${e.path}" not found. Exiting…`)
-  process.exit(1)
-})
+async function start () {
+  const setupFiles = Promise.all([
+    'config.toml',
+    'addresses.txt',
+    'SECRETKEY'
+  ].map(f => readFile(join(BASEDIR, f), 'utf8')))
 
-const [config, addresses, secretkey] = await setupFiles
+  setupFiles.catch((e) => {
+    console.error(`File "${e.path}" not found. Exiting…`)
+    process.exit(1)
+  })
 
-const { asset, airdrop } = toml.parse(config)
-asset.distributorKeypair = Keypair.fromSecret(secretkey.trim())
+  const [config, addresses, secretkey] = await setupFiles
 
-const server = new Server(HORIZON_URL)
-const networkPassphrase = Networks[NETWORK]
-const validateAccount = accountValidator(server, asset)
-const sendAirdrop = paymentSender(server, networkPassphrase, asset, airdrop.amount)
+  const { asset, airdrop } = toml.parse(config)
+  asset.distributorKeypair = Keypair.fromSecret(secretkey.trim())
 
-const accountIDs = addresses.split(/\r\n|\r|\n/)
-                            .map(s => s.trim())
-                            .filter(s => !!s)
-                            .filter(s => !s.startsWith('#'))
-console.log(`Hardcore validation of ${accountIDs.length} addresses happening. Just a sec`)
-const validatedAccountIDs = await Promise.all(accountIDs.map(validateAccount))
-const [validAccounts, invalidAccounts] = partition(validatedAccountIDs, acct => acct.success)
+  const server = new Server(HORIZON_URL)
+  const networkPassphrase = Networks[NETWORK]
+  const validateAccount = accountValidator(server, asset)
+  const sendAirdrop = paymentSender(server, networkPassphrase, asset, airdrop.amount)
 
-const timestamp = Date.now()
+  const accountIDs = addresses.split(/\r\n|\r|\n/)
+                              .map(s => s.trim())
+                              .filter(s => !!s)
+                              .filter(s => !s.startsWith('#'))
+  console.log(`Hardcore validation of ${accountIDs.length} addresses happening. Just a sec`)
+  const validatedAccountIDs = await Promise.all(accountIDs.map(validateAccount))
+  const [validAccounts, invalidAccounts] = partition(validatedAccountIDs, acct => acct.success)
 
-const VALIDATION_ERROR_REPORT = join(BASEDIR, 'reports', `validation-errors-${timestamp}.csv`)
-await writeReport(VALIDATION_ERROR_REPORT, invalidAccounts, ['address', 'fedAddress', 'reason'])
-console.log(`Found ${validAccounts.length} valid accounts`)
-console.log(`Found problems with ${invalidAccounts.length} addresses. Errors logged in ${VALIDATION_ERROR_REPORT}\n`)
+  const timestamp = Date.now()
 
-if (validateOnly) {
-  process.exit()
-}
+  const VALIDATION_ERROR_REPORT = join(BASEDIR, 'reports', `validation-errors-${timestamp}.csv`)
+  await writeReport(VALIDATION_ERROR_REPORT, invalidAccounts, ['address', 'fedAddress', 'reason'])
+  console.log(`Found ${validAccounts.length} valid accounts`)
+  console.log(`Found problems with ${invalidAccounts.length} addresses. Errors logged in ${VALIDATION_ERROR_REPORT}\n`)
 
-console.log(`Sending airdrop to ${validAccounts.length} addresses\n`)
-// Send airdrops in series to avoid race conditions and other sequence number
-// fuckery.
-let airdropResults = []
-let airdropErrors = []
-for (let acct of validAccounts) {
-  const shortID = shortenAccountID(acct.address)
-
-  console.log(`Sending ${airdrop.amount} ${asset.code} to ${shortID}`)
-  const drop = await sendAirdrop(acct.address)
-  let status = {
-    ...acct,
-    ...drop,
-    result: drop.success ? 'success' : 'failed'
+  if (validateOnly) {
+    process.exit()
   }
 
-  airdropResults.push(status)
+  console.log(`Sending airdrop to ${validAccounts.length} addresses\n`)
+  // Send airdrops in series to avoid race conditions and other sequence number
+  // fuckery.
+  let airdropResults = []
+  let airdropErrors = []
+  for (let acct of validAccounts) {
+    const shortID = shortenAccountID(acct.address)
 
-  if (status.success) {
-    console.log(`${asset.code} successfully airdropped to ${shortID}`)
-    status.tx_link = `https://stellar.expert/explorer/${NETWORK.toLowerCase()}/tx/${status.tx_hash}`
-  } else {
-    console.log(`Airdrop to ${shortID} failed`)
-    airdropErrors.push(status)
+    console.log(`Sending ${airdrop.amount} ${asset.code} to ${shortID}`)
+    const drop = await sendAirdrop(acct.address)
+    let status = {
+      ...acct,
+      ...drop,
+      result: drop.success ? 'success' : 'failed'
+    }
+
+    airdropResults.push(status)
+
+    if (status.success) {
+      console.log(`${asset.code} successfully airdropped to ${shortID}`)
+      status.tx_link = `https://stellar.expert/explorer/${NETWORK.toLowerCase()}/tx/${status.tx_hash}`
+    } else {
+      console.log(`Airdrop to ${shortID} failed`)
+      airdropErrors.push(status)
+    }
+
+    console.log()
   }
 
-  console.log()
+  const AIRDROP_RESULTS_REPORT = join(BASEDIR, 'reports', `airdrop-results-${timestamp}.csv`)
+  const AIRDROP_ERROR_LOGS = join(BASEDIR, 'logs', `airdrop-errors-${timestamp}.json`)
+
+  await writeReport(AIRDROP_RESULTS_REPORT, airdropResults, ['address', 'fedAddress', 'result', 'tx_link', 'reason'])
+  console.log(`Airdrop finished. Results logged in ${AIRDROP_RESULTS_REPORT}`)
+
+  if (airdropErrors.length) {
+    await writeLog(AIRDROP_ERROR_LOGS, airdropErrors)
+    console.log(`Some errors were encountered. More info in ${AIRDROP_ERROR_LOGS}`)
+  }
 }
 
-const AIRDROP_RESULTS_REPORT = join(BASEDIR, 'reports', `airdrop-results-${timestamp}.csv`)
-const AIRDROP_ERROR_LOGS = join(BASEDIR, 'logs', `airdrop-errors-${timestamp}.json`)
-
-await writeReport(AIRDROP_RESULTS_REPORT, airdropResults, ['address', 'fedAddress', 'result', 'tx_link', 'reason'])
-console.log(`Airdrop finished. Results logged in ${AIRDROP_RESULTS_REPORT}`)
-
-if (airdropErrors.length) {
-  await writeLog(AIRDROP_ERROR_LOGS, airdropErrors)
-  console.log(`Some errors were encountered. More info in ${AIRDROP_ERROR_LOGS}`)
-}
+start()
